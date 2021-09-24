929fa4d83f80378e3a5d6c16e8350cda48d751b7
bautiercoli <bautistaercoli7@gmail.com>
2021-09-23 21:29:55 -0300

    added js

--- js/main.js
+++ js/main.js
@@ -1,15 +1,13 @@
-const productos_disponibles = 45
-const limite_maximo = 3
-const productos_agotados = 6
-for (let i = 1; i <= productos_disponibles; i++) {
-    
-    if (i >= limite_maximo) {
-        break;
-    }
-    else if (i === productos_agotados) {
-        continue;
-    }
-    
-    let nombreIngresado = prompt("Ingrese que productos que desea comprar");
-    alert (`${nombreIngresado} sera el prducto Nº ${i}`); 
-}
+// en el desafio hice como que una cantidad de amigos van a comprar en el sitio, y tienen que poner cual es el precio final y cuantos son, para que luego les diga cuanto tiene que pagar cada uno.
+const porcentaje = 0.05
+let totalCompra = parseInt(prompt("Ingresar valor total de la compra de los productos"))
+let cantidadAmigos = parseInt(prompt("Con cuantos amigos va a realizar la compra?"))
+const sumar = (a,b) => a + b;
+const restar = (a,b) => a - b;
+const multiplicar = (a,b) => a * b;
+const dividir = (a,b) => a / b;
+const potencia = (a,b) => a ** b;
+
+let precioFinal = dividir(multiplicar(totalCompra,porcentaje),restar(1,potencia(sumar(1,porcentaje),-cantidadAmigos)));
+
+alert(`cada uno va a pagar ${precioFinal} pesos cada uno`);
