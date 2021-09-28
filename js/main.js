7ae30bae78ce4db4d54f8c3976574e0b87be1d34
bautiercoli <bautistaercoli7@gmail.com>
2021-09-28 17:53:23 -0300

    added some js

--- js/main.js
+++ js/main.js
@@ -1,13 +1,18 @@
-// en el desafio hice como que una cantidad de amigos van a comprar en el sitio, y tienen que poner cual es el precio final y cuantos son, para que luego les diga cuanto tiene que pagar cada uno.
-const porcentaje = 0.05
-let totalCompra = parseInt(prompt("Ingresar valor total de la compra de los productos"))
-let cantidadAmigos = parseInt(prompt("Con cuantos amigos va a realizar la compra?"))
-const sumar = (a,b) => a + b;
-const restar = (a,b) => a - b;
-const multiplicar = (a,b) => a * b;
-const dividir = (a,b) => a / b;
-const potencia = (a,b) => a ** b;
+let producto = prompt("Ingrese el producto");
+let tipoProducto = prompt("Ingrese el tipo de producto");
+let cantidad = prompt("Ingrese la cantidad deseada");
 
-let precioFinal = dividir(multiplicar(totalCompra,porcentaje),restar(1,potencia(sumar(1,porcentaje),-cantidadAmigos)));
-
-alert(`cada uno va a pagar ${precioFinal} pesos cada uno`);
+class productos{
+    constructor(producto, tipoProducto, cantidad) {
+        this.nombre = producto;
+        this.tipo = tipoProducto;
+        this.cantidad = cantidad;
+    }
+    guardar(){
+    console.log("El producto es: " + this.nombre); 
+    console.log("El tipo es: " + this.tipo); 
+    console.log("La cantidad es: " + this.cantidad);
+    }
+}
+const producto1 = new productos(producto, tipoProducto, cantidad);
+producto1.guardar();
