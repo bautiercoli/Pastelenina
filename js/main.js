9d277f166f7aa5487802524a26d08f1ab7f4ae52
bautiercoli <bautistaercoli7@gmail.com>
2021-10-01 17:40:29 -0300

    added some js

--- js/main.js
+++ js/main.js
@@ -1,18 +1,29 @@
-let producto = prompt("Ingrese el producto");
-let tipoProducto = prompt("Ingrese el tipo de producto");
-let cantidad = prompt("Ingrese la cantidad deseada");
+let totalCompra = parseInt(prompt("Ingresar valor total de la compra de los productos"))
+let cantidadAmigos = parseInt(prompt("Con cuantos amigos va a realizar la compra?"))
+const sumar = (a,b) => a + b;
+const restar = (a,b) => a - b;
+const multiplicar = (a,b) => a * b;
+const dividir = (a,b) => a / b;
+const potencia = (a,b) => a ** b;
 
-class productos{
-    constructor(producto, tipoProducto, cantidad) {
-        this.nombre = producto;
-        this.tipo = tipoProducto;
-        this.cantidad = cantidad;
-    }
-    guardar(){
-    console.log("El producto es: " + this.nombre); 
-    console.log("El tipo es: " + this.tipo); 
-    console.log("La cantidad es: " + this.cantidad);
+let precioFinal = dividir(multiplicar(totalCompra,porcentaje),restar(1,potencia(sumar(1,porcentaje),-cantidadAmigos)));
+
+alert(`cada uno va a pagar ${Math.floor(precioFinal)} pesos cada uno`);
+
+class Pastelenina{
+constructor (Nombre, Precio, Cantidad){
+this.Nombre = Nombre;
+this.Precio = Precio;
+this.Cantidad = Cantidad;
     }
 }
-const producto1 = new productos(producto, tipoProducto, cantidad);
-producto1.guardar();
+
+let activoPastelenina1 = [];
+
+activoPastelenina1.push (new Pastelenina ("Cheesecake", "Torta", 1200, 1));
+activoPastelenina1.push (new Pastelenina ("Cookies", "Galletitas", 900, 1));
+activoPastelenina1.push (new Pastelenina ("LetterCake", "Torta", 1400, 1));
+activoPastelenina1.push (new Pastelenina ("Brownies", "Torta", 1000, 1));
+
+const Cheesecake = activoPastelenina1.slice(1, 2);
+console.log(activoPastelenina1);
